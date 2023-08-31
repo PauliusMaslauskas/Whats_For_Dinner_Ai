6e7efa281ffb65e9e9e774a5d0a5101f8a999691
pmaslauskas <paulius.mass@gmail.com>
2023-08-31 12:14:49 +0300

    Initial commit

---
+++ src/main/java/com/example/whats_for_dinner_ai/WhatsForDinnerAiApplication.java
@@ -0,0 +1,13 @@
+package com.example.whats_for_dinner_ai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhatsForDinnerAiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WhatsForDinnerAiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/whats_for_dinner_ai/WhatsForDinnerAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.whats_for_dinner_ai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhatsForDinnerAiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
